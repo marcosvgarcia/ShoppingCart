86772498216d44299fb67612c1c707891da93359
Marcos Vazquez <marcosvazquezgarcia@outlook.es>
2015-04-12 19:28:44 +0200

    Estructura
    Con una lista de precios fija, he creado la clase ShoppingCart, con
    constructor y metodos que definan las frutas y los precios y sus sumas.

--- ShoppingCart.rb
+++ ShoppingCart.rb
@@ -1,10 +1,28 @@
-require "rubygems"
 
 class ShoppingCart
 
-	def initialize(fruit, price)
-	@fruit = fruit
+	def initialize(cart)
+	@cart = cart
+	end
+
+	attr_accessor :name, :price
+
+	def fruit(name, price)
+	@name = name
 	@price = price
 	end
 
+	def Fruit_Total_Price(name.times, price)
+	return name.times * price
+	end
+
+	def Cart_Total_Price(Fruit_Total_Price)
+	puts (Fruit_Total_Price).reduce(:+)
 end
+
+
+apple = Fruit.new(apple, 10)
+orange = Fruit.new(orange, 5)     
+grapes = Fruit.new(grapes, 15)
+banana = Fruit.new(banana, 20)
+watermelon = Fruit.new(watermelon, 50)
